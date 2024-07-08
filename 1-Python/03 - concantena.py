58afea2c031154a0be69bc62f2f1c29763ea74e5
Xanpis <wellington.org.com@gmail.com>
2024-07-08 17:40:59 -0300

    up

---
+++ 1-Python/03 - concantena.py
@@ -0,0 +1,14 @@
+
+name = input("Enter the name of the game \n")
+yearLaunch = int(input("Enter the year of the game \n")) 
+gamePrice = float(input("Enter the price of the game \n"))
+planIncluded = input("it's included in the plan \n")
+
+print("## Game data ##")
+print("================")
+
+print("\n Name of the game = ",name, "\n Years of the game = ", yearLaunch,
+      "\n Price of the game = ",gamePrice, "\n It's included plan = ",planIncluded)
+
+# Way with f string
+print(f"\n Name of the game = {name}\n Year of the game = {yearLaunch}\n Price of the game = {gamePrice}\n It's included plan = {planIncluded}")
