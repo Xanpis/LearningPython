2990da9458af22d735737d09a4aaef7f937b1f44
Xanpis <wellington.org.com@gmail.com>
2024-07-08 16:59:49 -0300

    up

---
+++ 1-PythonPrint/02 - input.py
@@ -0,0 +1,19 @@
+'''
+multi-line comment
+gameTeca 
+
+'''
+
+name = input("Enter the name of the game \n")
+# convert from string
+yearLaunch = int(input("Enter the year of the game \n")) 
+gamePrice = float(input("Enter the price of the game \n"))
+planIncluded = input("it's included in the plan \n")
+
+print("\n")
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
+
+
