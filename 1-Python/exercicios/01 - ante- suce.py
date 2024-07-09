a14d59926a8898c8343f2c2ad9cfc22560cc3236
Xanpis <wellington.org.com@gmail.com>
2024-07-09 11:54:40 -0300

    up

---
+++ 1-Python/exercicios/01 - ante- suce.py
@@ -0,0 +1,16 @@
+# Antecessor e Successor
+
+num = int(input("Enter the number\n"))
+
+print(f"Antecessor { num -1 } Successor { num + 1} ")
+
+
+# Media das notas
+ 
+num1 = int(input("Enter the grade (1)\n"))
+num2 = int(input("Enter the grade (2)\n"))
+num3 = int(input("Enter the grade (3)\n"))
+num4 = int(input("Enter the grade (4)\n"))
+num5 = num1 + num2 + num3 + num4
+ 
+print(f"Media { num5/4}")
