bb5ac1647e8887c3855727b7fa08c74e6a93f32d
Xanpis <wellington.org.com@gmail.com>
2024-07-09 11:16:29 -0300

    up

---
+++ 1-Python/04 - operadores.py
@@ -0,0 +1,19 @@
+
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+
+sum = num1 + num2
+sub = num1 - num2 
+div = num1 / num2
+mut = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+
+# Arithmetic operator 
+
+print(f"Remainder of dividing {num1} by number {num2} is {mod}")
+print(f"power of number {num1} by number {num2} is {exp}")
+
+
+
+
