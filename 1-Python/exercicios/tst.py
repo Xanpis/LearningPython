7b4ed6e859379f7a533eb6591c9918cb29e3332b
Xanpis <wellington.org.com@gmail.com>
2024-07-12 11:42:37 -0300

    up

--- 1-Python/exercicios/tst.py
+++ 1-Python/exercicios/tst.py
@@ -1,7 +1,10 @@
 gameName = ["Resident evil 4", "Star wars Jedi survivor", 
 "The Legend of Zelda", "Red dead 2", "Mario odyssey"]
 
-line = "="
-print(gameName[-2:])
+# line = "="
+# print(gameName[-2:])
+# print(line * 25)
 
-print(line * 25)
+name = "teste12"
+
+print(gameName[::-2])
