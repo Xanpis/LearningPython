2a9badbcb67b39d687598e171f0e14b0a5fc9890
Xanpis <wellington.org.com@gmail.com>
2024-07-14 17:35:01 -0300

    up

--- 1-Python/19 - listaComprehension.py
+++ 1-Python/19 - listaComprehension.py
@@ -6,16 +6,16 @@
 # Mesmo que o for mas com comprehension
 
 
-preList = [i for i in range(10) if 1 < 4 ]
+preList = [i for i in range(10) if i < 4 ]
 # print(preList)
 
 # Verificando se há a letra a na lista
 gameList = ['Mario Odyssey', 'Dk country', 'Luigi Mansion', 'Kirby']
 newList = [x for x in gameList if 'a' in x ]
-# print(newList)
+print(newList)
 
-tu = []
-for i in gameList:
-   if 'a' in i:
-     tu.append(i)
-print(tu)
+# tu = []
+# for i in gameList:
+#    if 'a' in i:
+#      tu.append(i)
+# print(tu)
---
+++ 1-Python/exercicios/04 - contagem.py
@@ -0,0 +1,7 @@
+
+# contagem regressiva
+x = 10
+
+while(x > 0):
+   x -= 1
+
