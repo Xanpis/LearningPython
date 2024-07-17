77320fe244d3f578ee77d47ab187344040410e7c
Xanpis <wellington.org.com@gmail.com>
2024-07-17 07:42:12 -0300

    up

---
+++ 1-Python/24 - funcaoLambda.py
@@ -0,0 +1,21 @@
+# Potencia 
+power = lambda x: x **2
+print(power(4))
+print(25*'=')
+
+# Verificar se o numero é par 
+par = lambda i: i % 2 == 0
+print(par(7))
+print(25*'=')
+
+# Dividir
+div = lambda y ,x: y / x   
+print(div(22,6))
+print(25*'=')
+
+# Invertendo uma String
+reverse = lambda s : s[::-1]
+print(reverse('Abelha'))
+
+
+
--- 1-Python/exercicios/tst.py
+++ 1-Python/exercicios/tst.py
@@ -1,14 +1,20 @@
-gameName = ["Resident evil 4", "Star wars Jedi survivor", 
-"The Legend of Zelda", "Red dead 2", "Mario odyssey"]
+# gameName = ["Resident evil 4", "Star wars Jedi survivor", 
+# "The Legend of Zelda", "Red dead 2", "Mario odyssey"]
 
-# line = "="
-print(gameName[:-2])
-# print(line * 25)
+# # line = "="
+# print(gameName[:-2])
+# # print(line * 25)
 
-# name = "teste12"
+# # name = "teste12"
 
-# print(gameName[::-2])
+# # print(gameName[::-2])
 
-# listaJogos = "Fifa23", "Star Wars", "The Legend of Zelda", "Red Dead 2"
-# print (lista) Jogos-1
+# # listaJogos = "Fifa23", "Star Wars", "The Legend of Zelda", "Red Dead 2"
+# # print (lista) Jogos-1
 
+def string_para_tuplas(s):
+    tuplas = tuple((letra) for letra in s)
+    print(tuplas[3])
+
+# Exemplo de uso:
+string_para_tuplas("exemplo")
