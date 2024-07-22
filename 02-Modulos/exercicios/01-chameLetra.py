27fb44492e47c55e839865df8f85c2c8999c2d08
Xanpis <wellington.org.com@gmail.com>
2024-07-22 16:52:39 -0300

    up

---
+++ 02-Modulos/exercicios/01-chameLetra.py
@@ -0,0 +1,6 @@
+import mdLetras
+
+print(mdLetras.inverte('gasolinA'))
+print(mdLetras.par('01234567'))
+print(mdLetras.impar('01234567'))
+
---
+++ 02-Modulos/exercicios/mdLetras.py
@@ -0,0 +1,11 @@
+def inverte(letra):
+   letra = letra[::-1]
+   return(letra)
+
+def par(letra):
+   letra = letra[::2]
+   return(letra)
+
+def impar(letra):
+   letra = letra[1::2]
+   return(letra)
