fe4ab595c4c7d901daec9bccebaa0719cc1b3bb1
Xanpis <wellington.org.com@gmail.com>
2024-07-23 15:46:07 -0300

    up

---
+++ 02-Modulos/02 - os.py
@@ -0,0 +1,18 @@
+import os
+
+# imprimindo pasta atual
+print(os.getcwd())
+
+# lista arquivos e pastas 
+print(os.listdir())
+
+# versão do sistema operacional 
+# os.system('uname -a')
+
+# Mostra informação do sistrema
+os.system('lsb_release -a')
+
+# memoria 
+os.system('free -h')
+
+# os.system('clear')
