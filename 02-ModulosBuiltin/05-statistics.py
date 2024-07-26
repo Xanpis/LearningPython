b95ef4e6ffeefead7f199e355cedefc8370524cb
Xanpis <wellington.org.com@gmail.com>
2024-07-26 17:26:15 -0300

    up

--- 02-ModulosBuiltin/05-statistics.py
+++ 02-ModulosBuiltin/05-statistics.py
@@ -6,6 +6,7 @@ print(statistics.mean([2,5,7,8,4,9]))
 # 2 - Mediana (ordena o numero e pega o do meio)
 print(statistics.median([3,5,7,10,10,12,16,17]))
 
+
 # o numero que mais se repete é a moda 
 print(statistics.mode([6,7,8,7,9,8,]))
 
