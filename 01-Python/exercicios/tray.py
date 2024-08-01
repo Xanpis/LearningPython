97f2c1094bdadb3cbfb3aa608531b4b1bc82766e
Xanpis <wellington.org.com@gmail.com>
2024-08-01 19:09:01 -0300

    up

--- 01-Python/exercicios/tray.py
+++ 01-Python/exercicios/tray.py
@@ -5,13 +5,22 @@ def nu():
     print(f'Números { ite} e {flo}')
 
 
-cap = True
-while(cap == True):
+# cap = True
+# while(cap == True):
+#     b = ''
+#     try:
+#         nu()
+#     except Exception as ero:
+#         print(f'Erro Valor invalido {ero.__cause__}')
+#         b = ero.__cause__
+#     if(b!= None):
+#         cap == False
 
+
+cap = True
+while cap:
     try:
-        nu()
+        nu()  # Chama a função que pode lançar uma exceção
+        cap = False  # Sai do loop se `nu()` for executado sem erros
     except Exception as ero:
-        print(f'Erro Valor invalido {ero.__cause__}')
-        if(ero.__cause__!= None):
-            cap == False
-    
+        print(f'Erro: Valor inválido - {str(ero)}')
