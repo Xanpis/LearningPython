6eb2a8bbcf8ea5f916991a9f12a0bdf6c74dbd8c
Xanpis <wellington.org.com@gmail.com>
2024-08-02 10:21:24 -0300

    up

--- 01-Python/exercicios/testNOvo.py
+++ 01-Python/exercicios/testNOvo.py
@@ -2,3 +2,20 @@ name = "fifa23"
 
 print(len(name))
 print(name[:6])
+
+gameDict = {
+   'name': 'Fifa',
+   'years': 2023,
+   'gamePrice': 99.20,
+   'classification': 8.1,
+   'genre':['Esporte', 'Familia'],
+    'gas':{'vas', 'mas'}
+
+}
+
+
+nom = gameDict.items()
+for no,na in gameDict.items():
+    print(f'Chave = {no} Valor = {na}')
+
+print(nom)
