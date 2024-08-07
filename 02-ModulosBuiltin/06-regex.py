c5d6080eb286db8331f7541bd6c178848f81276b
Xanpis <wellington.org.com@gmail.com>
2024-08-07 16:34:14 -0300

    up

--- 02-ModulosBuiltin/06-regex.py
+++ 02-ModulosBuiltin/06-regex.py
@@ -11,3 +11,29 @@ print('final = ',mat.end())
 site = 'http://www.google.com'
 found = re.search(r'\.',site)
 print(found)
+
+# buscando uma lista de caractere dentro de uma frase, buscando de (a até m) no alfabeto 
+text = 'Transformando pessoas em programadores sem limites'
+pad = "[a-m]"
+result = re.findall(pad,text)
+print(text)
+print(result)
+
+# buscando a primeira letra 
+find = r'^A'
+phrase = ['A casa está limpa','O dia começou lindo ', 'Vamos passear']
+for f in phrase:
+   if re.match(find,f):
+      print("Corresponde = ",f)
+   else:
+      print('Nao Corresponde = ',f)
+
+
+# verificando se tem no final de uma string
+find = r'!$'
+phrase2 = 'vagabond!'
+result = re.search(find,phrase2)
+if result:
+   print('Contem')
+else:
+   print('Não tem')
