4605f5f82fea0b9079c02c20371488dfa7c3bd57
Xanpis <wellington.org.com@gmail.com>
2024-08-11 16:01:50 -0300

    up

--- 02-ModulosBuiltin/08-json.py
+++ 02-ModulosBuiltin/08-json.py
@@ -15,11 +15,9 @@ print(type(pessoa_json))
 # Imprimindo no Formato json
 print(json.dumps(pessoa_dict, indent=4, sort_keys=True))
 
-
 # Salvando txt 
 with open('pessoa.txt','w') as file:
    json.dump(pessoa_dict,file)
-
 
 # Abrindo aquivo json
 with open('iris.json', 'r') as f:
