716fbfcc56b2dbf3e2cb37b463bbbfe9bf5a11fb
Xanpis <wellington.org.com@gmail.com>
2024-08-12 18:07:58 -0300

    up

--- 02-ModulosBuiltin/exercicios/test.py
+++ 02-ModulosBuiltin/exercicios/test.py
@@ -1,5 +1,6 @@
 import re
+# pegando a palavra inteira de um conjunto de string
 text = "He was carefully disguised butb captured quickly by police."
-tu = re.findall(r"\w+r\b", text)
-
+tu = re.findall(r"\w+e \b", text)
 print(tu)
+
